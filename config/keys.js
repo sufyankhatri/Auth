0bce38e7cd95c3ffe1c62545baa537e6bdc12166
sufyankhatri <43412206+sufyankhatri@users.noreply.github.com>
2019-09-08 03:48:55 +0500

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    'mongodb+srv://sufyan:k1h2a3t4r5i6@cluster0-bslgw.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//DB config
+const db = require('./config/keys').MongoURI;
+
+//connect to mongo
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('mongo db connected'))
+  .catch(err => console.log(err));
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
